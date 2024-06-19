77c2020287d3782a589979a6f1efa92bf3876de8
Farid Mammadbayov <mm.farid@post.cz>
2024-06-19 10:39:29 +0200

    messages container fix

--- src/server.ts
+++ src/server.ts
@@ -72,6 +72,8 @@ io.on("connection", (socket: socketio.Socket) => {
   });
 });
 
-server.listen(PORT, () => {});
+server.listen(PORT, () => {
+  console.log("Server started");
+});
 
 export default app;
